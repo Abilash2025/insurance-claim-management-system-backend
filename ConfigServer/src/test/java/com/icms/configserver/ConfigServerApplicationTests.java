8fbc57afc005b74bbd44917af869d96571931c2c
ABILASH RAVI <114767381+Abilash2025@users.noreply.github.com>
2026-08-14 16:51:33 +0530

    Addde ConfigServer and EurekaServer

---
+++ ConfigServer/src/test/java/com/icms/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icms.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
